e01921c61bcf80623ef471131987dbef2f50c283
LilMike707 <michaelmarracq@yahoo.com>
2023-09-19 11:05:11 -0700

    asdasd

---
+++ dogfinder/src/DogList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function DogList({dogs}) {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>
+            HELLOZ. WE HAVE DOGZ. CLICK ON THEM FOR MORE INFO.
+          </h1>
+        </div>
+      </div>
+      <div>
+        {dogs.map(d => (
+          <div key={d.name}>
+            <img src={d.src} alt={d.name} />
+            <h3>
+              <Link to={`/dogs/${d.name.toLowerCase()}`}>{d.name}</Link>
+            </h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DogList;
